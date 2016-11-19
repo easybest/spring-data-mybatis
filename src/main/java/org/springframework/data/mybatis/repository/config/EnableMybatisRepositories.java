db5c640682fb7deb9249368c631e25c945f0e502
Jarvis <hapcars@gmail.com>
2016-11-19 10:53:06 +0800

    closed #9 : support count query method

--- src/main/java/org/springframework/data/mybatis/repository/config/EnableMybatisRepositories.java
+++ src/main/java/org/springframework/data/mybatis/repository/config/EnableMybatisRepositories.java
@@ -91,7 +91,7 @@ public @interface EnableMybatisRepositories {
 
     /**
      * Returns the key of the {@link QueryLookupStrategy} to be used for lookup queries for query methods. Defaults to
-     * {@link QueryLookupStrategy.Key#CREATE_IF_NOT_FOUND}.
+     * {@link org.springframework.data.repository.query.QueryLookupStrategy.Key#CREATE_IF_NOT_FOUND}.
      *
      * @return QueryLookupStrategy.Key
      */
