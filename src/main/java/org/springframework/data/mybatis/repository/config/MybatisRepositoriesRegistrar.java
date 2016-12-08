db4970514b1a11161d203a2acee80d4144ebb655
JARVIS SONG <iamjarvissong@gmail.com>
2016-12-08 19:28:01 +0800

    Closed #30

--- src/main/java/org/springframework/data/mybatis/repository/config/MybatisRepositoriesRegistrar.java
+++ src/main/java/org/springframework/data/mybatis/repository/config/MybatisRepositoriesRegistrar.java
@@ -18,10 +18,14 @@
 
 package org.springframework.data.mybatis.repository.config;
 
+import org.springframework.beans.factory.support.BeanDefinitionRegistry;
+import org.springframework.context.EnvironmentAware;
 import org.springframework.context.ResourceLoaderAware;
+import org.springframework.core.env.Environment;
 import org.springframework.core.io.ResourceLoader;
-import org.springframework.data.repository.config.RepositoryBeanDefinitionRegistrarSupport;
-import org.springframework.data.repository.config.RepositoryConfigurationExtension;
+import org.springframework.core.type.AnnotationMetadata;
+import org.springframework.data.repository.config.*;
+import org.springframework.util.Assert;
 
 import java.lang.annotation.Annotation;
 
@@ -30,9 +34,10 @@ import java.lang.annotation.Annotation;
  *
  * @author Jarvis Song
  */
-class MybatisRepositoriesRegistrar extends RepositoryBeanDefinitionRegistrarSupport implements ResourceLoaderAware {
+class MybatisRepositoriesRegistrar extends RepositoryBeanDefinitionRegistrarSupport implements ResourceLoaderAware, EnvironmentAware {
 
     private ResourceLoader resourceLoader;
+    private Environment    environment;
 
     @Override
     protected Class<? extends Annotation> getAnnotation() {
@@ -48,5 +53,34 @@ class MybatisRepositoriesRegistrar extends RepositoryBeanDefinitionRegistrarSupp
     public void setResourceLoader(ResourceLoader resourceLoader) {
         super.setResourceLoader(resourceLoader);
         this.resourceLoader = resourceLoader;
+    }
+
+    @Override
+    public void setEnvironment(Environment environment) {
+        this.environment = environment;
+        super.setEnvironment(environment);
+    }
+
+    @Override
+    public void registerBeanDefinitions(AnnotationMetadata annotationMetadata, BeanDefinitionRegistry registry) {
+        Assert.notNull(resourceLoader, "ResourceLoader must not be null!");
+        Assert.notNull(annotationMetadata, "AnnotationMetadata must not be null!");
+        Assert.notNull(registry, "BeanDefinitionRegistry must not be null!");
+
+        // Guard against calls for sub-classes
+        if (annotationMetadata.getAnnotationAttributes(getAnnotation().getName()) == null) {
+            return;
+        }
+
+        MybatisAnnotationRepositoryConfigurationSource configurationSource = new MybatisAnnotationRepositoryConfigurationSource(
+                annotationMetadata, getAnnotation(), resourceLoader, environment);
+
+        RepositoryConfigurationExtension extension = getExtension();
+        RepositoryConfigurationUtils.exposeRegistration(extension, registry, configurationSource);
+
+        RepositoryConfigurationDelegate delegate = new RepositoryConfigurationDelegate(configurationSource, resourceLoader,
+                environment);
+
+        delegate.registerRepositoriesIn(registry, extension);
     }
 }
