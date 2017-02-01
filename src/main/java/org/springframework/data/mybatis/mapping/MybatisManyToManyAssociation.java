af9631fb5ecc2e8015d251e5eec3f2c896970c56
Jarvis Song <4566733+easybest@users.noreply.github.com>
2017-02-01 18:45:36 +0800

    closed #94 #95 support @OneToMany

--- src/main/java/org/springframework/data/mybatis/mapping/MybatisManyToManyAssociation.java
+++ src/main/java/org/springframework/data/mybatis/mapping/MybatisManyToManyAssociation.java
@@ -8,4 +8,11 @@ public class MybatisManyToManyAssociation extends MybatisOneToManyAssociation {
     public MybatisManyToManyAssociation(MybatisPersistentProperty inverse, MybatisPersistentProperty obverse) {
         super(inverse, obverse);
     }
+
+    public boolean preferJoinTable() {
+        if (null != joinColumn) {
+            return false;
+        }
+        return true;
+    }
 }
