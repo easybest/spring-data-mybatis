0a51534838353e6f4cf19cff5556e1eb2473fb85
Jarvis Song <4566733+easybest@users.noreply.github.com>
2018-11-26 10:35:12 +0800

    change MyBatis to Mybatis

--- spring-data-mybatis-boot-starter/src/main/java/org/springframework/data/mybatis/autoconfigure/MyBatisRepositoriesAutoConfiguration.java
+++ spring-data-mybatis-boot-starter/src/main/java/org/springframework/data/mybatis/autoconfigure/MyBatisRepositoriesAutoConfiguration.java
@@ -1,5 +1,6 @@
 package org.springframework.data.mybatis.autoconfigure;
 
+import javax.sql.DataSource;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
@@ -7,22 +8,23 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
-import org.springframework.data.mybatis.repository.MyBatisRepository;
-import org.springframework.data.mybatis.repository.config.MyBatisRepositoryConfigExtension;
-import org.springframework.data.mybatis.repository.support.MyBatisRepositoryFactoryBean;
-
-import javax.sql.DataSource;
+import org.springframework.data.mybatis.repository.MybatisRepository;
+import org.springframework.data.mybatis.repository.config.MybatisRepositoryConfigExtension;
+import org.springframework.data.mybatis.repository.support.MybatisRepositoryFactoryBean;
 
 /**
- * {@link EnableAutoConfiguration Auto-configuration} for Spring Data's MyBatis Repositories.
+ * {@link EnableAutoConfiguration Auto-configuration} for Spring Data's MyBatis
+ * Repositories.
  * 
  * @author Jarvis Song
  */
 @Configuration
 @ConditionalOnBean(DataSource.class)
-@ConditionalOnClass(MyBatisRepository.class)
-@ConditionalOnMissingBean({ MyBatisRepositoryFactoryBean.class, MyBatisRepositoryConfigExtension.class })
-@ConditionalOnProperty(prefix = "spring.data.mybatis.repositories", name = "enabled", havingValue = "true",
-		matchIfMissing = true)
+@ConditionalOnClass(MybatisRepository.class)
+@ConditionalOnMissingBean({ MybatisRepositoryFactoryBean.class,
+		MybatisRepositoryConfigExtension.class })
+@ConditionalOnProperty(prefix = "spring.data.mybatis.repositories", name = "enabled", havingValue = "true", matchIfMissing = true)
 @Import(MyBatisRepositoriesAutoConfigureRegistrar.class)
-public class MyBatisRepositoriesAutoConfiguration {}
+public class MyBatisRepositoriesAutoConfiguration {
+
+}
