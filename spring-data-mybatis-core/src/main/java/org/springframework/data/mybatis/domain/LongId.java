f2cad47aa9b426c1a18e3e4ce8aa41664aec5f6d
Jarvis Song <hapcars@gmail.com>
2018-11-27 13:42:56 +0800

    add condition query support

--- spring-data-mybatis-core/src/main/java/org/springframework/data/mybatis/domain/LongId.java
+++ spring-data-mybatis-core/src/main/java/org/springframework/data/mybatis/domain/LongId.java
@@ -1,22 +1,24 @@
 package org.springframework.data.mybatis.domain;
 
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
+import java.io.Serializable;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.MappedSuperclass;
-import java.io.Serializable;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
 @MappedSuperclass
 public class LongId implements Serializable {
 
-	@GeneratedValue(strategy = GenerationType.AUTO) @Id protected Long id;
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Id
+	protected Long id;
 
 	public LongId(Long id) {
 		this.id = id;
 	}
+
 }
