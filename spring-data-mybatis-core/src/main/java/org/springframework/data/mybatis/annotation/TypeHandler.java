f08699c5eb84a8cee208ff1268eb9a62918adb71
Jarvis Song <hapcars@gmail.com>
2018-12-21 00:56:21 +0800

    closed #182

--- spring-data-mybatis-core/src/main/java/org/springframework/data/mybatis/annotation/TypeHandler.java
+++ spring-data-mybatis-core/src/main/java/org/springframework/data/mybatis/annotation/TypeHandler.java
@@ -3,6 +3,7 @@ package org.springframework.data.mybatis.annotation;
 import static java.lang.annotation.ElementType.ANNOTATION_TYPE;
 import static java.lang.annotation.ElementType.FIELD;
 import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.ElementType.PARAMETER;
 
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
@@ -12,7 +13,7 @@ import java.lang.annotation.Target;
  * @author Jarvis Song
  */
 @Retention(RetentionPolicy.RUNTIME)
-@Target(value = { FIELD, METHOD, ANNOTATION_TYPE })
+@Target(value = { FIELD, METHOD, ANNOTATION_TYPE, PARAMETER })
 public @interface TypeHandler {
 
 	String value();
